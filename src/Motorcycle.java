de87928276d99422edb9dec9beecf33c2cd11cec
SVorn <stephan@vornefeld.net>
2025-12-18 13:38:17 +0100

    more changes

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -1,12 +1,13 @@
 public class Motorcycle extends Vehicle{
 
+    //Attributes
+    Boolean isPillionRiderCapable;
+
+
     //Constructor is accessing constructor of vehicle superclass
     Motorcycle(String model, int mileage, Boolean isPillionRiderCapable){
         super(model, mileage);
         this.isPillionRiderCapable = isPillionRiderCapable;
     }
-
-    //Attributes
-    Boolean isPillionRiderCapable;
 
 }
